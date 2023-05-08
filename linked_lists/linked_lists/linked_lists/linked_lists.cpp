f5bb27e4f536d710a01581c9af292b0a10005895
fidelisaboke <fidelisaboke@gmail.com>
2023-05-08 12:39:38 +0300

    Updated linked_lists.cpp

--- linked_lists/linked_lists/linked_lists/linked_lists.cpp
+++ linked_lists/linked_lists/linked_lists/linked_lists.cpp
@@ -4,6 +4,18 @@
 #include <iostream>
 using namespace std;
 
+//Singly linked lists
+struct singly_linked_node {
+    int data;
+    struct singly_linked_node* next;
+};
+
+struct doubly_linked_node {
+    int data;
+    struct doubly_linked_node* previous;
+    struct doubly_linked_node* next;
+};
+
 int main()
 {
     return 0;
