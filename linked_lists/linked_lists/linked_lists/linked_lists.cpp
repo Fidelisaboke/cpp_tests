b33c5faf82c8f40f4abc53cc5f3baeb8f004dc23
fidelisaboke <fidelisaboke@gmail.com>
2023-05-08 20:30:38 +0300

    Updated linked_lists.cpp

--- linked_lists/linked_lists/linked_lists/linked_lists.cpp
+++ linked_lists/linked_lists/linked_lists/linked_lists.cpp
@@ -1,15 +1,15 @@
-// linked_lists.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//Creation of types of linked lists in C++
 
 #include <iostream>
 using namespace std;
 
-//Singly linked lists
+//Singly linked list
 struct singly_linked_node {
     int data;
     struct singly_linked_node* next;
 };
 
+//Doubly linked list
 struct doubly_linked_node {
     int data;
     struct doubly_linked_node* previous;
